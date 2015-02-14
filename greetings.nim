a227682759250af7352b8e702064e52712b5336d
patrick nsukami <ndkpatt@gmail.com>
2015-02-14 21:46:09 +0000

    case statement.

--- greetings.nim
+++ greetings.nim
@@ -2,9 +2,12 @@
 echo("What's your name? ")
 var name: string = readLine(stdin)
 
-if name == "":
-  echo("poor soul")
-elif name == "name"  :
-  echo("funny")
+case name
+of "":
+  echo("Poor soul, you lost your name?")
+of "name":
+  echo("Very funny, your name is name.")
+of "Dave", "Frank":
+  echo("Cool name!")
 else:
   echo("Hi, ", name, "!")
