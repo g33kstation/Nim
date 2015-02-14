54ea1e311eea935f54ec8fcf9134ad807b2e1c89
patrick nsukami <ndkpatt@gmail.com>
2015-02-14 22:03:52 +0000

    Do nothing statement.

--- greetings.nim
+++ greetings.nim
@@ -11,3 +11,13 @@ of "Dave", "Frank":
   echo("Cool name!")
 else:
   echo("Hi, ", name, "!")
+
+
+from strutils import parseInt
+
+echo("A number please: ")
+let n = parseInt(readLine(stdin))
+case n
+of 0..2, 4..7: echo("n in the set: {0,1,2,3,4,5,6,7}")
+of 3,8: echo("n is 3 or 8")
+else: discard
