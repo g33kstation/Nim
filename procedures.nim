db290af0012b61c90f12cf8cb63ff38612cef0e2
patrick nsukami <ndkpatt@gmail.com>
2015-02-16 02:36:46 +0000

    If mutable var is needed inside proc, declare it w/ var in the proc body.

--- procedures.nim
+++ procedures.nim
@@ -18,6 +18,16 @@ proc sumTillNegative(x: varargs[int]):int =
       return
     result = result + i
 
-echo sumTillNegative()
-echo sumTillNegative(3,4,5)
-echo sumTillNegative(3,4,-1)
+# echo sumTillNegative()
+# echo sumTillNegative(3,4,5)
+# echo sumTillNegative(3,4,-1)
+
+proc printSeq(s: seq, nprinted: int = -1): int =
+  var nprinted = if nprinted == -1: s.len else: min(nprinted, s.len)
+  for i in 0 .. <nprinted:
+    echo s[i]
+
+# numeric is a seq
+var numeric = @[9,9]
+echo numeric.len
+echo printSeq(numeric)
